2e2a7a23f4a81833a23605319c3022d089e3ee49
Sam Hewitt <hewittsamuel@gmail.com>
2014-06-14 22:26:45 -0400

    Refinement.

--- fedora-post-install-script.sh
+++ fedora-post-install-script.sh
@@ -21,6 +21,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, see <https://www.gnu.org/licenses/gpl-3.0.txt>
 
+clear
 echo ''
 echo '#-------------------------------------#'
 echo '#     Fedora Post-Install Script      #'
